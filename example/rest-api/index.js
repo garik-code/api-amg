598f4ccd37400d689b2a5e2578a0a083ae83790a
Игорь Мещеряков <maiL@garik.site>
2019-09-24 02:13:49 +0300

    example

--- example/rest-api/index.js
+++ example/rest-api/index.js
@@ -11,8 +11,40 @@ const Api = new Amg({
   }
 })
 
-Api.reg('test@example.com', 'rootroot', 'root')
-.then(
-  success => console.log(success),
-  err     => console.log(err)
-)
+const express = require('express')
+const fileUpload = require('express-fileupload')
+const rateLimit = require('express-rate-limit')
+const routeCache = require('route-cache')
+const cors = require('cors')
+const app = express()
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  message: 'Activity detected. Your IP address is blocked for 15 minutes.'
+})
+const brutalLimiter = rateLimit({
+  windowMs: 60 * 60 * 1000,
+  max: 5,
+  message: 'Activity detected. Your IP address is blocked for 1 hour.'
+})
+
+app.use(cors())
+app.use(fileUpload())
+app.use(limiter)
+app.use('/auth', brutalLimiter)
+app.use('/reg', brutalLimiter)
+
+// app.get('/news/:token', routeCache.cacheSeconds(1), (req, res, next) => {
+//   News.search(req.params.token)
+//   .then(
+//     json => res.send(json),
+//     err => res.send(err)
+//   )
+// })
+
+// Api.reg('test@example.com', 'rootroot', 'root')
+// .then(
+//   success => console.log(success),
+//   err     => console.log(err)
+// )
